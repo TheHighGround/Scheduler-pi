e8ee8d3ebf125ca55a1bb13919357fefeb50b99e
Jeremy Williams <31394110+TheHighGround@users.noreply.github.com>
2018-12-22 19:33:49 -0700

    schedule.js

---
+++ app.js
@@ -0,0 +1,23 @@
+let startUp = 
+["Fire Breath", "Meditate Goals", "Build Checklist", "Thought Exercise"];
+console.log(startUp[0]);
+
+let timeIncrements =
+    {quarter: 15,
+half: 30,
+full: 60}; 
+console.log(timeIncrements.quarter);
+
+let workOut =
+["Calisthenics", "Weights", "Yoga"];
+console.log(workOut[0]);
+
+let food =
+["Meals", "Goal Calories", "Actual Calories"]; 
+console.log(food[0]);
+ 
+ let nutrition =
+ {protien: 0,
+ fat: 0,
+ carbs: 0};
+console.log(nutrition.protien);
